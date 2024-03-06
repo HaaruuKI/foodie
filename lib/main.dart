0b129e6ba9d16897d9937937a3ef8e23273a2187
HaaruuKI <antonio.gallegos.palafox@gmail.com>
2024-03-05 17:05:59 -0700

    Refactor Firebase initialization and options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:foodie/domain/services/firebase_options.dart';
 import 'package:foodie/ui/colors.dart';
 import 'package:foodie/ui/router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+  // print('Platform: ${DefaultFirebaseOptions.currentPlatform}');
 }
 
 class MyApp extends StatelessWidget {
