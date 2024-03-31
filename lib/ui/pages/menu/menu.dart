1b422664d95ff931f22d5cdca94286d8aaeaf238
AnthonyPy1511 <viceten.antonio.gallegos@gmail.com>
2024-03-30 20:23:35 -0700

    Refactor import statements for AddToFavorites class

--- lib/ui/pages/menu/menu.dart
+++ lib/ui/pages/menu/menu.dart
@@ -5,7 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:foodie/domain/entities/add_to_favorites.dart';
+import 'package:foodie/domain/entities/favorite/add_to_favorites.dart';
 import 'package:foodie/domain/entities/log_in.dart';
 import 'package:foodie/domain/entities/send_data_to_realtime.dart';
 import 'package:foodie/domain/entities/snack_bar_send_data.dart';
