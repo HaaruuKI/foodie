cd92695cb38cd6480f7f09b187e76032e0dca59a
AnthonyPy1511 <viceten.antonio.gallegos@gmail.com>
2024-03-31 14:38:40 -0700

    Fix typo in get_moneyfoodie_user constant

--- lib/domain/entities/name_log_in.dart
+++ lib/domain/entities/name_log_in.dart
@@ -6,5 +6,5 @@ class GetNameLogIn {
   static const String get_lastname_user = 'lastname';
   static const String get_email_user = 'email';
   static const String get_number_user = 'numero';
-  static const String get_moneyfoodie_user = 'mooneyfoodie';
+  static const String get_moneyfoodie_user = 'moneyfoodie';
 }
